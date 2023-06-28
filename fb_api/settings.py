7598aa43cf746ddbfb1942f4052323a7c03c052d
Gina Nikroo <gina.nikroo@gmail.com>
2023-06-28 13:45:21 +0200

    Add gunicorn to requirements

--- fb_api/settings.py
+++ fb_api/settings.py
@@ -65,12 +65,11 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
-    '127.0.0.1',
 ]
 
 # Application definition
